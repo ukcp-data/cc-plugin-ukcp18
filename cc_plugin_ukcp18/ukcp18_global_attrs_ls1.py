b14573608fe8b09e65c4c46c6d2362dbdf498652
Ag Stephens <ag.stephens@stfc.ac.uk>
2018-04-04 15:11:10 +0000

    Updated plugin

--- cc_plugin_ukcp18/ukcp18_global_attrs_ls1.py
+++ cc_plugin_ukcp18/ukcp18_global_attrs_ls1.py
@@ -44,3 +44,58 @@ class UKCP18GlobalAttrsLS1Check(BaseNCCheck):
                                                     level="HIGH",
                                                     vocabulary_ref="ukcp:ukcp18")(ds)
     
+    def check_gatls1_004(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': 'CF-1\\.5', 'attribute': 'Conventions'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_005(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': '.{4,}', 'attribute': 'source'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_006(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': 'UKCP18', 'attribute': 'project'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_007(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': '.{4,}', 'attribute': 'contact'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_008(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': '.{4,}', 'attribute': 'references'},
+                                                    level="MEDIUM",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_009(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': 'v\\d{8}', 'attribute': 'version'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_010(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': '.{4,}', 'attribute': 'title'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_011(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': '\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.*', 'attribute': 'creation_date'},
+                                                    level="MEDIUM",
+                                                    vocabulary_ref="")(ds)
+    
+    def check_gatls1_012(self, ds):
+        return checklib.checks.GlobalAttrVocabCheck(kwargs={'attribute': 'institution_id', 'vocab_lookup': 'label'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="ukcp:ukcp18")(ds)
+    
+    def check_gatls1_013(self, ds):
+        return checklib.checks.GlobalAttrRegexCheck(kwargs={'regex': 'Met Office Hadley Centre \\(MOHC\\), FitzRoy Road, Exeter, Devon, EX1 3PB, UK\\.', 'attribute': 'institution'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="ukcp:ukcp18")(ds)
+    
+    def check_gatls1_014(self, ds):
+        return checklib.checks.GlobalAttrVocabCheck(kwargs={'attribute': 'domain', 'vocab_lookup': 'canonical_name'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="ukcp:ukcp18")(ds)
+    
